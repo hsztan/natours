f33fff7a2802671df4f3b4cb57a8fd59d2231ef0
Henry Nawrocki <hdev@sztanski.com>
2021-04-06 09:20:44 -0500

    adds helmet middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,9 +11,14 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
+app.use(helmet());
+
+// Development login
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Limit requests from same API
 const limiter = rateLimit({
   max: 3,
   windowMs: 60 * 60 * 1000,
@@ -20,7 +26,9 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb' }));
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
